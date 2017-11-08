50920d979b9fe173f8ba42c1ad6d073300a583f4
Poy Chang <poy.chang@seec.com.tw>
2017-11-08 08:56:06 +0800

    測試功能：修改當前頁面的 DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,23 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { title = `PoyChang's Chrome Extension`; }
+export class AppComponent {
+  title = `PoyChang's Chrome Extension`;
+
+  click() {
+    console.log('將此網站(http://www.cnblogs.com/artech/)的文章版面清乾淨 ;)');
+
+    chrome.tabs.query({active: true, currentWindow: true}, tab => {
+      chrome.tabs.executeScript(
+          tab[0].id, {
+            code: `
+        document.getElementById('sideBar').remove();
+        document.getElementById('myRewards').remove();
+        document.getElementById('div_digg').remove();
+        document.getElementsByClassName('forFlow')[0].style.marginLeft = 0;
+        `
+          },
+          function(response) {});
+    });
+  }
+}
